c9f25c58302acbd4f163871f8bde3421578d5a64
Jakob <jakob.dickert@gmx.de>
2015-08-15 14:29:39 +0000

    Initial Commit

---
+++ KeyboardControler/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  KeyboardControler
+//
+//  Created by Jakob on 15/08/2015.
+//  Copyright (c) 2015 Jakob. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
